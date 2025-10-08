847dc050e90879af94ba049e24c8075fac909b5b
Manjaro-control <dragonhejun@gmail.com>
2025-10-08 17:03:08 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["45.194.34.8"];
+pub const RS_PUB_KEY: &str = "I4MlUPCQGGl+It9gfNnb8my7ejXuqhelneakek3ugUo=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
